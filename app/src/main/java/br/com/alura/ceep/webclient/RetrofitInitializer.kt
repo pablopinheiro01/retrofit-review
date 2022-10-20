e9c19123483e3d734422f77fc0383a31324c1322
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-10-19 21:41:07 -0300

    Adicionado interceptador de log

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInitializer.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInitializer.kt
@@ -1,15 +1,30 @@
 package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.service.NotaService
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RetrofitInitializer {
 
-    private var retrofit = Retrofit.Builder()
+    private val client by lazy{
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    val retrofit = Retrofit.Builder()
         .baseUrl("http://192.168.15.90:8080/")
+        .client(client)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     val notaService = retrofit.create(NotaService::class.java)
+
+
+
+
 }
