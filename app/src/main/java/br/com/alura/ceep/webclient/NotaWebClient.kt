a87d7f20b051754d99e85d54399036de7f8f999d
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-10-20 20:25:46 -0300

    feito tratamento para o caso de exceção não bloquear a navegação do app

--- app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -4,18 +4,27 @@ import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.webclient.model.NotaResponse
+import br.com.alura.ceep.webclient.service.NotaService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+private const val TAG = "NotaWebClient"
 class NotaWebClient {
 
-    suspend fun buscaTodas(): List<Nota> {
+    private val notaService: NotaService = RetrofitInitializer().notaService
 
-        return RetrofitInitializer().notaService.buscaTodasCoroutines().map { nota ->
-            nota.nota
+    suspend fun buscaTodas(): List<Nota>?{
+
+        return try {
+            notaService.buscaTodasCoroutines().map { nota ->
+                nota.nota
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "erro: $e")
+            null
         }
     }
 
