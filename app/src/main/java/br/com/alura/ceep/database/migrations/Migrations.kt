d8edb989fd2a65b0268a17dfc04c21125a622ee0
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-10-25 19:45:02 -0300

    Alterando a estrutura de dados da tabela, inserindo um novo campo para indicar quando a nota foi sincronizada com a api

--- app/src/main/java/br/com/alura/ceep/database/migrations/Migrations.kt
+++ app/src/main/java/br/com/alura/ceep/database/migrations/Migrations.kt
@@ -57,3 +57,10 @@ val MIGRATION_1_2 = object : Migration(1, 2){
     }
 
 }
+
+val MIGRATION_2_3 = object : Migration(2,3){
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE Nota ADD synchronized INTEGER NOT NULL DEFAULT 0")
+    }
+
+}
--- app/src/main/java/br/com/alura/ceep/model/Nota.kt
+++ app/src/main/java/br/com/alura/ceep/model/Nota.kt
@@ -1,5 +1,6 @@
 package br.com.alura.ceep.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
@@ -10,5 +11,11 @@ data class Nota(
     val id: String = UUID.randomUUID().toString(),
     val titulo: String,
     val descricao: String,
-    val imagem: String? = null
+    val imagem: String? = null,
+
+    //setado os objetos existentes na base com o valor default de false
+    // o tipo Bool nao e suportado pelo sqlite por isso o uso de Strings
+    //0 = false e 1 = true
+    @ColumnInfo(defaultValue = "0")
+    val synchronized: Boolean = false
 )
