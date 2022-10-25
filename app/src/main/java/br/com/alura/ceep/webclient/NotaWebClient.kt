63361a1ba5c5b895315ebad13246c5911f4b21a2
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-10-25 19:28:55 -0300

    Criado a suspend fun para testes de request no service salvando dados

--- app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -3,6 +3,7 @@ package br.com.alura.ceep.webclient
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.model.NotaRequest
 import br.com.alura.ceep.webclient.model.NotaResponse
 import br.com.alura.ceep.webclient.service.NotaService
 import kotlinx.coroutines.Dispatchers
@@ -25,6 +26,26 @@ class NotaWebClient {
         } catch (e: Exception) {
             Log.e(TAG, "erro: $e")
             null
+        }
+    }
+
+    suspend fun salva(nota: Nota) {
+        try{
+            val req = notaService.salva(
+                nota.id,
+                NotaRequest(
+                    titulo = nota.titulo,
+                    imagem = nota.imagem,
+                    descricao = nota.descricao
+                )
+            )
+            if(req.isSuccessful){
+                Log.i(TAG, "Sucesso BB")
+            }else{
+                Log.i(TAG, "Tem parada errada ai irmao ${req.code()}")
+            }
+        }catch (e: Exception){
+            Log.e(TAG, "Erro gerado: $e")
         }
     }
 
